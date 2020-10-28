679cb28ef3596ad2be4d6ce1a77b2dfed3e734c0
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-10-28 18:33:55 +0500

    Added Store and Slice

---
+++ src/store/productSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export interface Product {
+    id: string
+    title: string
+    description: string
+    price: number
+    imageUrl: string
+    added?: boolean
+}
+
+const INITIAL_STATE: Product[] = [{
+    id: "123",
+    title: "Blue t-shirt",
+    description: "No fancy sizing charts here, one t-shirt size to rule them all",
+    imageUrl: "/blue-tshirt.png",
+    price: 399
+},
+{
+    id: "456",
+    title: "Yellow t-shirt",
+    description: "This unique t-shirt is guaranteed to fit nobody, not even new born babies",
+    imageUrl: "/yellow-tshirt.png",
+    price: 499
+},
+{
+    id: "789",
+    title: "Red t-shirt",
+    description: "The only product on our site that might actually be worth buying",
+    imageUrl: "/red-tshirt.png",
+    price: 799
+}
+]
+
+export const productSlice = createSlice({
+    name: "Product",
+    initialState:INITIAL_STATE,
+    reducers:{
+        addProduct:(state,action)=>{
+            
+        }
+    }
+});
+
+export const {addProduct}=productSlice.actions
+export default productSlice.reducer
